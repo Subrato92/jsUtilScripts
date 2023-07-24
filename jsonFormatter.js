83390737bfbd26108a16a4f01d732561740e0097
SUBRATO MONDAL <subrato.mondal18@gmail.com>
2023-07-24 15:41:45 +0530

    first commit

---
+++ jsonFormatter.js
@@ -0,0 +1,24 @@
+
+process.setUncaughtExceptionCaptureCallback( e => {
+    console.error("Uncaught Exception : ", e);
+});
+
+const str = "{\"a\":\"a1\", \"b\":\"b1\", \"c\":[\"c1\",\"c2\"]}";
+
+const patterns = [
+    /"[\w]+":"[\w\d\s]*"/g, 
+    /"[\w]+":\[[\w\d\s,"]*\]/g
+];
+
+let finalMatchedResult = [];
+
+for(pattern of patterns){
+    console.log("processing pattern : %s", pattern);
+    matchedResult = str.match(pattern);
+    console.log("   matchedResult(s) : %s \n", JSON.stringify(matchedResult));
+    finalMatchedResult = finalMatchedResult.concat(matchedResult);
+}
+
+let formattedJson = "{ \n\t"+finalMatchedResult.join(",\r\n\t")+ "\n}"
+
+console.log(formattedJson);
